25a434acb84eb2de0f39153d01a408910167bb5c
SetarehGhorshi <ssghorsh@uwaterloo.ca>
2024-07-25 00:29:12 -0400

    pk added

--- precompile/contracts/decryption/decryption.go
+++ precompile/contracts/decryption/decryption.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 
 	enc "github.com/FairBlock/DistributedIBE/encryption"
+	"github.com/drand/kyber"
 	bls "github.com/drand/kyber-bls12381"
 	"github.com/ethereum/go-ethereum/precompile"
 	"github.com/ethereum/go-ethereum/precompile/abi"
@@ -11,21 +12,49 @@ import (
 
 type Decryption struct {
 	precompile.StatefulPrecompiledContract
+	pk []byte
 }
 func NewDecryption() *Decryption {
 	return &Decryption{
 		StatefulPrecompiledContract: precompile.NewStatefulPrecompiledContract(
 			abi.DecryptionABI,
 		),
+		pk: []byte{},
 	}
 }
+func (con *Decryption) GetPK(ctx precompile.StatefulContext) ([]byte,error){
+	return con.pk,nil;
+}
+func (con *Decryption) SetPK(ctx precompile.StatefulContext, _pk []byte) (bool,error) {
+	suite := bls.NewBLS12381Suite()
+	pkPoint := suite.G1().Point()
+	err := pkPoint.UnmarshalBinary(_pk)
+	if err != nil {
+		return false,err;
+	}
+	con.pk = _pk;
+	return true,nil;
+}
 
-func (con Decryption) Decrypt(ctx precompile.StatefulContext, privateKeyByte []byte, cipherBytes []byte) ([]byte, error) {
+func (con *Decryption) Decrypt(ctx precompile.StatefulContext, privateKeyByte []byte, cipherBytes []byte, id string) ([]byte, error) {
 	suite := bls.NewBLS12381Suite()
 	privateKeyPoint := suite.G2().Point()
 	err := privateKeyPoint.UnmarshalBinary(privateKeyByte)
 	if err != nil {
 		return []byte{},err
+	}
+	pkPoint := suite.G1().Point()
+	_ = pkPoint.UnmarshalBinary(con.pk)
+	hG2, ok := suite.G2().Point().(kyber.HashablePoint)
+	if !ok {
+		panic("invalid point")
+	}
+	idByte := []byte(id)
+	Qid := hG2.Hash(idByte)
+	p1 := suite.Pair(pkPoint,Qid)
+	p2 := suite.Pair(suite.G1().Point().Base(), privateKeyPoint)
+	if !p1.Equal(p2){
+		return []byte{},nil
 	}
 	var destPlainText bytes.Buffer
 	var cipherBuffer bytes.Buffer
@@ -33,7 +62,7 @@ func (con Decryption) Decrypt(ctx precompile.StatefulContext, privateKeyByte []b
 	if err != nil {
 		return []byte{},err
 	}
-	err = enc.Decrypt(privateKeyPoint, privateKeyPoint, &destPlainText, &cipherBuffer)
+	err = enc.Decrypt(pkPoint, privateKeyPoint, &destPlainText, &cipherBuffer)
 	if err != nil {
 		return []byte{},err
 	}
--- precompile/contracts/interfaces/IDecryption.sol
+++ precompile/contracts/interfaces/IDecryption.sol
@@ -7,7 +7,8 @@ pragma solidity ^0.8.24;
 /// @title Provides aggregators and their users methods for configuring how they participate in L1 aggregation.
 /// @notice Precompiled contract 
 interface IDecryption {
-
-    function decrypt(bytes calldata privateKeyByte, bytes calldata cipherBytes) external view returns (bytes memory);
+    function getPK() external view returns (bytes memory);
+    function setPK(bytes calldata _pk) external returns (bool);
+    function decrypt(bytes calldata privateKeyByte, bytes calldata cipherBytes, string calldata id) external view returns (bytes memory);
 
 }
